eaff357456c615f22d360e157ec48b383c17886e
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-08-18 19:34:07 +0100

    Added user and comment table and add user_ref to the comment table

---
+++ db/migrate/20220818183012_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.string :title
+      t.text :text
+      t.integer :comments_counter
+      t.integer :likes_counter
+
+      t.timestamps
+    end
+  end
+end
