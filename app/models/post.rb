0c5c8e6991dce8fc6c305bde93999602756236f8
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-08-20 16:50:00 +0100

    Configure routes

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,11 @@ class Post < ApplicationRecord
   has_many :comments, class_name: 'Comment', foreign_key: :post_id
 
   after_save :update_posts_counter
+
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def update_posts_counter
     author.increment!(:posts_counter)
   end
@@ -12,8 +17,4 @@ class Post < ApplicationRecord
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
-
-  validates :title, length: { maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
