978c3a6c0ae3cbb7870f73c3be02c4b8927e263e
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-08-23 16:23:54 +0100

    Add test for controllers

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,16 +2,28 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    it 'returns http success' do
-      get '/posts/index'
-      expect(response).to have_http_status(:success)
+    before(:example) { get '/users/1/posts' }
+    it 'responds with the code 200' do
+      expect(response).to have_http_status(:ok)
+    end
+    it "renders 'index' template" do
+      expect(response).to render_template(:index)
+    end
+    it 'renders correct contents' do
+      expect(response.body).to include('Here is a list of posts for a given user')
     end
   end
 
   describe 'GET /show' do
-    it 'returns http success' do
-      get '/posts/show'
+    before(:example) { get '/users/1/posts' }
+    it 'responds with the code 400' do
       expect(response).to have_http_status(:success)
+    end
+    it "renders 'show' view" do
+      expect(response).to render_template(locals: 'posts/show')
+    end
+    it 'renders contents in the view' do
+      expect(response.body).to include('Here is a list of posts for a given user')
     end
   end
 end
