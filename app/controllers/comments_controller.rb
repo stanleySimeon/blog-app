a73ea6900722aa3627739a8d6d14b8a5b6df8146
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-08-27 03:59:20 +0100

    Add forms to the app

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+    @post = Post.find(params[:post_id])
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @post = Post.find(params[:post_id])
+    @comment.author_id = current_user.id
+    @comment.post_id = params[:post_id]
+    if @comment.save
+      redirect_to user_posts_path(user_id: @post.author_id, id: @post.id), notice: 'Comment was successfully created.'
+    else
+      render :new, alert: 'Comment was not found.'
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
